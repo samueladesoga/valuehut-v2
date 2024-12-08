d1066cb6a9362f6f1906c4d769a24c9aaf38d454
Sam Adesoga <samueladesoga@gmail.com>
2024-12-08 23:32:09 +0100

    fixed valuehut string

--- app/components/FaqPage/Faq/Faq.tsx
+++ app/components/FaqPage/Faq/Faq.tsx
@@ -18,7 +18,7 @@ interface FAQProps {
 
 const FAQ: React.FC<FAQProps> = ({ faqs }) => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
-  const [activeCategory, setActiveCategory] = useState<string>("ValutHut");
+  const [activeCategory, setActiveCategory] = useState<string>("ValueHut");
 
   const categories = Array.from(new Set(faqs.map((faq) => faq.category)));
 
