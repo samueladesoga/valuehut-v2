7d0359e33101201e645bd12bc6ba9a16f6ad5d9f
m-irfan244 <irfan.baitham123@gmail.com>
2024-12-12 18:52:46 +0500

    Main updated

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
       <ServicesSection />
       <OurMission description="Helping organisations transform into a network of interdependent product teams across different business units." />
       <HeroBanner
-        title="Redefine the way you work."
-        subtitle="Helping organizations transform into a network of interdependent product teams across different sectors."
+        title="RTransforming Organisations and Teams."
+        subtitle="Your people are the heart of your organisation; hence, our mission is to help improve how people work, lead, and are led."
       />
       <ServicesOverview />
       <div className="py-6 sm:py-16">
