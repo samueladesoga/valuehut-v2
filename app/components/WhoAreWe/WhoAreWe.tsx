ff605f4c8756a33f2ecff428432139d186079c20
Sam Adesoga <samueladesoga@gmail.com>
2024-12-21 20:44:23 +0000

    fixed about us page

--- app/components/WhoAreWe/WhoAreWe.tsx
+++ app/components/WhoAreWe/WhoAreWe.tsx
@@ -5,13 +5,14 @@ function WhoAreWe() {
     <section className="md:flex justify-between items-start">
       <div className="w-full md:w-1/2 flex flex-col justify-between items-center md:items-start gap-8">
         <h1 className="text-8xl font-medium text-[#141834] font-primary text-center md:text-left">
-          Who are we
+          who are we
         </h1>
         <button className="hidden md:flex rounded-full bg-[#FF9F5A] px-6 py-3 text-main text-sm font-medium hover:bg-[#FF9F5A]/90">
           Get to know us better
         </button>
       </div>
-      <div className="flex flex-col justify-between gap-3">
+      <div className="w-full md:w-1/2 flex flex-col justify-between items-center md:items-start gap-10 md:gap-14  pt-8 md:pt-0">
+        <div className="flex flex-col justify-between gap-3">
           <h2 className="text-4xl font-semibold font-primary text-center text-main md:text-left">
             Our Vision
           </h2>
@@ -23,13 +24,12 @@ function WhoAreWe() {
             services.
           </p>
         </div>
-      <div className="w-full md:w-1/2 flex flex-col justify-between items-center md:items-start gap-10 md:gap-14  pt-8 md:pt-0">
         <div className="flex flex-col justify-between gap-3">
           <h2 className="text-4xl font-semibold font-primary text-center text-main md:text-left">
             Our Mission
           </h2>
           <p className="text-lg font-normal text-center font-secondary md:text-left">
-            Our coaches work with product teams to improve their
+          Our coaches work with product teams to improve their
             cross-functional and self-managing capabilities. Leaders are
             supported and coached to develop the capabilities required to create
             enabling work environments for these teams to succeed.
