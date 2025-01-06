656cded8c776d704b5334718a8adfa2e3030557c
Sam Adesoga <samueladesoga@gmail.com>
2025-01-06 15:27:22 +0000

    make button on hero banner optional

--- app/data/Home/HeroSection.ts
+++ app/data/Home/HeroSection.ts
@@ -1,8 +1,8 @@
 export interface HeroSectionProps {
   title: string;
   subtitle: string;
-  buttonText: string;
-  buttonLink: string;
+  buttonText?: string;
+  buttonLink?: string;
   showRating?: boolean;
   ratingText?: string;
   ratingDetails?: string;
@@ -13,8 +13,6 @@ export const heroData: HeroSectionProps = {
   title: "Transforming Organisations and Teams.",
   subtitle:
     "Your people are the heart of your organisation; hence, our mission is to help improve how people work, lead, and are led.",
-  buttonText: "Get Started Now",
-  buttonLink: "/get-started",
   showRating: true,
   ratingText: "Excellent",
   ratingDetails: "4.78 out of 5 based on 34 reviews",
