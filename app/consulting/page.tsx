1e890d47bceced267c3697dccbe4cc58aa529929
Sam Adesoga <samueladesoga@gmail.com>
2025-01-18 12:50:07 +0000

    fixed text on website

--- app/consulting/page.tsx
+++ app/consulting/page.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Consulting",
   description:
-    "ValueHut Consulting provide Agile and Product Management consulting services to help you achieve your business goals to our global clients - London, Lagos, Accra and other parts of the world.",
+    "ValueHut Consulting provides Agile and Product Management consulting services to help you achieve your business goals to our global clients - London, Lagos, Accra and other parts of the world.",
 };
 
 const Page: NextPage = () => {
