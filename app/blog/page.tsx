ba933dccde585fd025c27fd6612e5a47011d2a52
m-irfan244 <irfan.baitham123@gmail.com>
2025-02-01 18:19:37 +0500

    Fixed the Ads Image

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -14,12 +14,20 @@ export const metadata: Metadata = {
 export default async function Blogs() {
   const sortingOptions = ["Latest", "Oldest", "Most viewed"];
   const NewArticles = await getAllArticles();
-  const mainArticle = NewArticles.props.posts[0];
-  const sideArticles = NewArticles.props.posts.slice(1, 6);
+  const articlesToShow = NewArticles.props.posts.filter(
+    (article) => article.showInHomePage
+  );
+  const mainArticle = articlesToShow[0];
+
+  const sideArticles = articlesToShow.slice(1, 6);
+
+  const otherArticles = NewArticles.props.posts.filter(
+    (article) => article.showInHomePage === false
+  );
 
   return (
     <div className="bg-[#F5F5F5] py-7">
-      <div className="bg-[#f5f5f5] px-4 md:px-4 xl:px-0 container mt-[100px]">
+      <div className="bg-[#f5f5f5] px-4 md:px-4 lg:px-0 container mt-[100px]">
         <div>
           <h2 className="text-[47px] sm:text-9xl leading-[61px] font-primary  mb-9 text-main sm:font-normal font-medium">
             Featured Articles
@@ -38,7 +46,7 @@ export default async function Blogs() {
                 tag={mainArticle.tag}
               />
             </div>
-            <div className="grid grid-cols-1 w-full ">
+            <div className="grid grid-cols-1 w-full content-start">
               <h1 className="font-primary font-medium text-[26px] leading-[33px] sm:hidden mt-8 sm:mt-0">
                 Latest Articles
               </h1>
@@ -59,7 +67,7 @@ export default async function Blogs() {
       </div>
       <section className="mt-20">
         <ArticlesSection
-          articles={NewArticles.props.posts}
+          articles={otherArticles}
           sortingOptions={sortingOptions}
           defaultSortBy="Latest"
         />
