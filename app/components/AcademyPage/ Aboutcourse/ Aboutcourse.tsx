005e3eb23c4d44c8f98e1ccf4242b2f3f0b0236b
Sam Adesoga <samueladesoga@gmail.com>
2025-02-15 10:57:12 +0100

    fixed headers

--- app/components/AcademyPage/ Aboutcourse/ Aboutcourse.tsx
+++ app/components/AcademyPage/ Aboutcourse/ Aboutcourse.tsx
@@ -9,9 +9,9 @@ function Aboutcourse({ description, logo }: IAboutcourse) {
   return (
     <div className="flex flex-col-reverse md:flex-row justify-between items-start md:items-center gap-5 md:gap-40 ">
       <div className="flex flex-col gap-3">
-        <h1 className="text-6xl font-medium text-main font-primary">
+        <h2 className="text-6xl font-medium text-main font-primary">
           About this course
-        </h1>
+        </h2>
         <p className="text-sm font-secondary font-medium text-body">
           {description}
         </p>
