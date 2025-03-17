66a59b7fb86fd3ce5768a655beaafc71713abfc9
Sam Adesoga <samueladesoga@gmail.com>
2025-03-17 12:36:08 +0000

    added google tag manager for ads

--- app/components/GoogleTagManager/GoogleTagManager.tsx
+++ app/components/GoogleTagManager/GoogleTagManager.tsx
@@ -6,7 +6,7 @@ export const GoogleTagManager = () => {
     <>
       {/* Global site tag (gtag.js) - Google Analytics */}
       <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-975370317"/>
-        <Script>
+        <Script id="google-tag-manager">
             {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){window.dataLayer.push(arguments);}
