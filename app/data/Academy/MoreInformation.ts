90831ea867f00873d890ff7c5e97f7e177acd265
Sam Adesoga <samueladesoga@gmail.com>
2025-04-16 16:43:57 +0100

    fixed more infomation

--- app/data/Academy/MoreInformation.ts
+++ app/data/Academy/MoreInformation.ts
@@ -12,7 +12,7 @@ export const moreInformationData: IMoreInformation[] = [
     text: "The price for the class is dependent on your home address.",
   },
   {
-    text: "Attendees are able to claim 14 PDU credits from the Project Management Institute (PMI) after attending this two-day course.",
+    text: "Attendees are able to claim 7 PDU credits from the Project Management Institute (PMI)  for each day of training you attend with ValueHut Consulting.",
   },
   {
     text: "Click here for more reasons to train with ValueHut Consulting",
